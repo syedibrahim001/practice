1f882e8d1abe96d886af1254d675f71dcd993f8a
syed ibrahim <syedibrahim6230@gmail.comgit config --global user.email syedibrahim6230@gmail.comgit config--listgit config --global user.emailsyedibrahim6230@gmail.com>
2024-08-31 16:19:39 +0530

    added inheritance in feacture brnch

--- practice1.py
+++ practice1.py
@@ -215,7 +215,36 @@
 # f.write('i want to go to trip 123')
 # f.close()
 
-f=open('C:/Users/Syed Ibrahim/Desktop/.txt','w')
-print(f.write('ansar is serious'))
+# f=open('C:/Users/Syed Ibrahim/Desktop/.txt','w')
+# print(f.write('ansar is serious'))
 
-'abcdefg','vnjefvifv'
+
+# Python code to demonstrate how parent constructors
+# are called.
+
+# parent class
+class Person(object):
+
+    # __init__ is known as the constructor
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+# child class
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+
+        # invoking the __init__ of the parent class
+        Person.__init__(self, name, idnumber)
+
+# creation of an object variable or an instance
+a = Employee('Rahul', 886012, 200000, "Intern")
+
+# calling a function of the class Person using its instance
+a.display()
